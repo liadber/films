0ecc06f0679a7141de8bef00ece90abc772012bc
liadber <liadbercovich@gmail.com>
2024-02-24 23:00:33 +0200

    Feature: Using Material Grid (That based on Flexbox), Navbar, Form, Table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
-import {ListComponent} from "./components/list/list.component";
-import {CardComponent} from "./components/card/card.component";
 import {MatGridListModule} from "@angular/material/grid-list";
 import {MatCardModule} from "@angular/material/card";
+import {TableComponent} from "./components/table/table.component";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatListModule} from "@angular/material/list";
+import {FormComponent} from "./components/form/form.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatGridListModule, MatCardModule, CardComponent, ListComponent],
+  imports: [RouterOutlet, MatGridListModule, MatCardModule, FormComponent, TableComponent, MatSidenavModule, MatListModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
