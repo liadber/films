e8cf42b3a8470fd7994e79744668f2cde6aa0ebf
liadber <liadbercovich@gmail.com>
2024-02-24 16:32:30 +0200

    Feature: Card, List using Material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {ListComponent} from "./components/list/list.component";
+import {CardComponent} from "./components/card/card.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CardComponent, ListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
