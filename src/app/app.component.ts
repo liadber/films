b4bde983f92a8d2a92060219c1b7d66b40800b51
liadber <liadbercovich@gmail.com>
2024-02-24 20:19:34 +0200

    Feature: Flexible & Stretched

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {ListComponent} from "./components/list/list.component";
 import {CardComponent} from "./components/card/card.component";
+import {MatGridListModule} from "@angular/material/grid-list";
+import {MatCardModule} from "@angular/material/card";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CardComponent, ListComponent],
+  imports: [RouterOutlet, MatGridListModule, MatCardModule, CardComponent, ListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
